9ddb5990b9997a70d7d8cefe2422b2f1223eef27
Params07 <92327167+Params07@users.noreply.github.com>
2021-10-22 17:00:02 +0530

    Update database.js

--- database.js
+++ database.js
@@ -4,8 +4,7 @@ const pool = new Pool({
     password:"92fb031c3b406a7c20ec4413cae52f6833b72cfc4ffa659840222bf3fd7ded57",
     host : "ec2-3-228-78-248.compute-1.amazonaws.com",
     port:5432,
-    database:"dca0b44mm0co30",
-    URI:"postgres://lbcyvarmnremig:92fb031c3b406a7c20ec4413cae52f6833b72cfc4ffa659840222bf3fd7ded57@ec2-3-228-78-248.compute-1.amazonaws.com:5432/dca0b44mm0co30
-"
+    database:"dca0b44mm0co30"
+   
 });
 module.exports = pool;
