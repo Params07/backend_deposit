9bbbf0413ab8e3e0c6a1f9f00a84cd0ea031d69d
Params07 <92327167+Params07@users.noreply.github.com>
2021-10-22 16:36:28 +0530

    Update database.js

--- database.js
+++ database.js
@@ -1,9 +1,11 @@
 const Pool = require("pg").Pool;
 const pool = new Pool({
-    user:"postgres",
-    password:"parama",
-    host : "localhost",
-    port:5433,
-    database:"project1"
+    user:"lbcyvarmnremig",
+    password:"92fb031c3b406a7c20ec4413cae52f6833b72cfc4ffa659840222bf3fd7ded57",
+    host : "ec2-3-228-78-248.compute-1.amazonaws.com",
+    port:5432,
+    database:"dca0b44mm0co30",
+    URI:"postgres://lbcyvarmnremig:92fb031c3b406a7c20ec4413cae52f6833b72cfc4ffa659840222bf3fd7ded57@ec2-3-228-78-248.compute-1.amazonaws.com:5432/dca0b44mm0co30
+"
 });
 module.exports = pool;
